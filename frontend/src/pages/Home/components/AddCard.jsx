5db125e5dfd315da2bf529df440414de176dd8f1
Vanessa Dmello <dmello.vanessa2001@gmail.com>
2023-11-10 20:58:44 +0530

    Add Routes  to HomePage

--- frontend/src/pages/Home/components/AddCard.jsx
+++ frontend/src/pages/Home/components/AddCard.jsx
@@ -4,8 +4,10 @@ import pink from "@mui/material/colors/pink";
 import Paper from "@mui/material/Paper";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
 import Typography from "@mui/material/Typography";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import { Link } from "@mui/material";
 
 export default function AddCard({ isCard }) {
 	return (
@@ -14,9 +16,7 @@ export default function AddCard({ isCard }) {
 				sx={{
 					display: "flex",
 					border: "2px solid #ec407a",
-					"&:hover": {
-						transform: "scale(1.05)",
-					},
+					
 					pl:
 						window.innerWidth > 900
 							? window.innerWidth > 1100
@@ -36,27 +36,35 @@ export default function AddCard({ isCard }) {
 				}}
 			>
 				{isCard ? (
-					<CardContent>
-						<AddCardIcon
-							sx={{
-								fontSize: 40,
-								color: pink[400],
-								pl: 2,
-							}}
-						/>
-						<Typography sx={{ fontSize: 15 }}>Add Card</Typography>
-					</CardContent>
+					<CardActionArea component="a" href="/addCard">
+						<CardContent>
+							<AddCardIcon
+								sx={{
+									fontSize: 40,
+									color: pink[400],
+									pl: 2,
+								}}
+							/>
+							<Typography sx={{ fontSize: 15 }}>
+								Add Card
+							</Typography>
+						</CardContent>
+					</CardActionArea>
 				) : (
-					<CardContent>
-						<CollectionsBookmarkIcon
-							sx={{
-								fontSize: 40,
-								color: pink[400],
-								pl: 2,
-							}}
-						/>
-						<Typography sx={{ fontSize: 15 }}>Add Deck</Typography>
-					</CardContent>
+					<CardActionArea component="a" href="/addDeck">
+						<CardContent>
+							<CollectionsBookmarkIcon
+								sx={{
+									fontSize: 40,
+									color: pink[400],
+									pl: 2,
+								}}
+							/>
+							<Typography sx={{ fontSize: 15 }}>
+								Add Deck
+							</Typography>
+						</CardContent>
+					</CardActionArea>
 				)}
 			</Card>
 		</Paper>
